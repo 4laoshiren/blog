8063baff02e8b3e6323f8b0b604704704811c145
zhongzhong <2322916463@qq.com>
2026-04-07 20:32:43 +0800

    chores

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,13 @@
 <template>
     <NuxtLayout>
-        <AppNavbar />
-            <UContainer>
-                <NuxtPage />
-            </UContainer>
-        <AppFooter class="w-full text-center py-4" />
+        <div class="flex min-h-screen">
+            <AppNavbar />
+            <div class="flex flex-col flex-1">
+                <UContainer class="flex-1">
+                    <NuxtPage />
+                </UContainer>
+                <AppFooter class="text-center py-4" />
+            </div>
+        </div>
     </NuxtLayout>
 </template>
--- app/components/App/Navbar.vue
+++ app/components/App/Navbar.vue
@@ -3,10 +3,17 @@
 </script>
 
 <template>
-  <nav>
+  <nav class="flex flex-col p-4">
+        <h1 class="text-2xl font-bold">zz4zz</h1>
         <NuxtLink to="/">Home</NuxtLink>
         <NuxtLink href="/articles">Articles</NuxtLink>
         <NuxtLink to="/projects">Projects</NuxtLink>
         <NuxtLink to="/bookmarks">Bookmarks</NuxtLink>
   </nav>
 </template>
+
+<style scoped>
+  nav {
+    font-family: DM Serif Display;
+  }
+</style>
