591276e1c968ce86b43e1afd711ab572e05d491f
zhongzhong <2322916463@qq.com>
2026-04-07 19:44:04 +0800

    remove iconify

--- app/components/Home/SocialLinks.vue
+++ app/components/Home/SocialLinks.vue
@@ -2,12 +2,8 @@
 
 <template>
     <h2>FIND ME ON</h2>
-    <NuxtLink target="_blank" to="https://space.bilibili.com/3493135850145934">
-        <span>bilibili</span>
-        <Icon name="streamline-logos:bilibili-logo-block" />
-    </NuxtLink>
-    <NuxtLink target="_blank" to="https://bandcamp.com/skeemaskofficial">
-        <span>bandcamp</span>
-        <Icon name="fa6-brands:bandcamp" />
-    </NuxtLink>
+    <a target="_blank" href="https://space.bilibili.com/3493135850145934">
+        bilibili
+    </a>
+    <a target="_blank" href="https://bandcamp.com/skeemaskofficial">bandcamp</a>
 </template>
