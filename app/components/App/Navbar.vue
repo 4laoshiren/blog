dba7e53b4d050314e3cb8e34fad2ba7f342144da
zhongzhong <2322916463@qq.com>
2026-04-08 12:43:37 +0800

    change AppArticleCard NavLink to absolute link

--- app/components/App/Navbar.vue
+++ app/components/App/Navbar.vue
@@ -4,7 +4,7 @@
     <nav class="sticky top-0 h-screen flex flex-col px-8 pt-20 gap-3">
         <NuxtLink class="text-3xl" to="/">zz4zz</NuxtLink>
         <NuxtLink class="hover:underline" to="/">Home</NuxtLink>
-        <NuxtLink class="hover:underline" href="/articles">Articles</NuxtLink>
+        <NuxtLink class="hover:underline" to="/articles">Articles</NuxtLink>
         <NuxtLink class="hover:underline" to="/projects">Projects</NuxtLink>
         <NuxtLink class="hover:underline" to="/bookmarks">Bookmarks</NuxtLink>
     </nav>
--- app/pages/articles/index.vue
+++ app/pages/articles/index.vue
@@ -3,12 +3,12 @@
 <template>
     <AppHeader title="Articles" description="Things I want to share." />
     <AppArticleCard
-        article_virtual_path="articles/building-your-first-api-with-expressjs-a-beginners-guide"
+        article_virtual_path="/articles/building-your-first-api-with-expressjs-a-beginners-guide"
         article_title="Building Your First API with Express.js: A Beginner's Guide"
         article_description="A beginner-friendly guide to building your first API with Express.js"
     />
     <AppArticleCard
-        article_virtual_path="articles/how-to-convert-a-svg-to-png-using-canvas"
+        article_virtual_path="/articles/how-to-convert-a-svg-to-png-using-canvas"
         article_title="How to convert a SVG to PNG using Canvas"
         article_description="A simple way to convert a SVG to PNG using Canvas"
     />
