2afd0b19fe2a8130f1cb9df1bfc4657ae00fcd1a
zhongzhong <2322916463@qq.com>
2026-04-08 11:26:28 +0800

    change website header to NuxtLink

--- app/components/App/Navbar.vue
+++ app/components/App/Navbar.vue
@@ -2,7 +2,7 @@
 
 <template>
     <nav class="flex flex-col px-8 pt-20 gap-3">
-        <h1 class="text-2xl font-bold">zz4zz</h1>
+        <NuxtLink class="text-2xl font-bold" to="/">zz4zz</NuxtLink>
         <NuxtLink class="hover:underline" to="/">Home</NuxtLink>
         <NuxtLink class="hover:underline" href="/articles">Articles</NuxtLink>
         <NuxtLink class="hover:underline" to="/projects">Projects</NuxtLink>
