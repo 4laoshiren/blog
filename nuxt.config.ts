dabdd0d57793792224cc9f3ea69a6d09b9ea9c80
zhongzhong <2322916463@qq.com>
2026-04-09 17:00:06 +0800

    switch font to just 'serif'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,8 @@ export default defineNuxtConfig({
             markdown: {
                 highlight: {
                     theme: "github-light",
-                }
-            }
-        }
+                },
+            },
+        },
     },
 });
