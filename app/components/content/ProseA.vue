2e1330450f47a6ac03f8a38c37871f6a68ca363e
zhongzhong <2322916463@qq.com>
2026-04-09 10:22:35 +0800

    重写nuxt/content的a标签

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(["href"]);
+</script>
+
+<template>
+    <a :href="props.href" target="_blank" class="hover:underline text-[#2e4c72]">
+        <slot />
+    </a>
+</template>
