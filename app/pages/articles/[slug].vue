c029893747af4fcbcf6a53d2621b119253a2ac7c
zhongzhong <2322916463@qq.com>
2026-05-08 16:23:11 +0800

    fix useAsyncData function key loss

--- app/pages/articles/[slug].vue
+++ app/pages/articles/[slug].vue
@@ -1,7 +1,7 @@
 <script setup>
 const route = useRoute();
 // 解包queryCollection的返回结果中的data属性, 重命名成articles, 语义上更好, 且与content.config.ts保持一致
-const { data: articles } = await useAsyncData(() =>
+const { data: articles } = await useAsyncData(route.path, () =>
     queryCollection("articles").path(route.path).first()
 );
 </script>
