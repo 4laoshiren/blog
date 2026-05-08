6de1d6e0b0021825626ea69037a4aa60133890ec
zhongzhong <2322916463@qq.com>
2026-05-08 21:09:13 +0800

    extract sunlit background from raw iframe to Vue component for a little bit SSR

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,7 @@
 <!-- 该项目中的layout仅抽离了"背景"这一feature -->
 <!-- 仅由一个iframe和slot构成，iframe置于slot图层下方 -->
-<script setup></script>
-
 <template>
     <!-- iframe不随页面滚动，置于其他图层下方，铺满，无边框 -->
-    <iframe src="/sunlit/index.html" class="fixed -z-10 w-full h-full border-none"></iframe>
+    <SunlitBackground class="-z-10" />
     <slot />
 </template>
