0784592b8a237f8590b31bf3696f0d17b39a3935
zhongzhong <2322916463@qq.com>
2026-05-11 12:08:49 +0800

    暂时取消背景以加快客户端渲染速度-2

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,6 +2,6 @@
 <!-- 仅由一个iframe和slot构成，iframe置于slot图层下方 -->
 <template>
     <!-- iframe不随页面滚动，置于其他图层下方，铺满，无边框 -->
-    <SunlitBackground class="-z-10" />
+    <!-- <SunlitBackground class="-z-10" /> -->
     <slot />
 </template>
